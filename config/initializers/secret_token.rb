ce2d05e37b5a19efd20daa592b2e1a985945b578
vagmi <vagmi.mudumbai@gmail.com>
2011-06-28 10:19:44 +0530

    add tasks and buckets model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasklist::Application.config.secret_token = 'af492af7773b27d77986f9c6c320cfabd42db824c71b276206148d4e228875a970ddee59a9f532744eedae7bb30f3b7fd49e662dafbe18d1565678a6a706a910'
